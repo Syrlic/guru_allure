3bc8019a3fe72801bde8c04faa1004905f9a16c3
Syrlic <oklonina@gmail.com>
2021-07-31 14:20:23 +0400

    test lambda v.0.2 test passed

--- src/test/java/tests/LambdaAllureTests.java
+++ src/test/java/tests/LambdaAllureTests.java
@@ -4,12 +4,14 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
 public class LambdaAllureTests {
 
+    public static final String REPOSITORY = "Syrlic/guru_allure";
+    public static final String NUMBER = "1";
     @BeforeAll
     public static void setup(){
         Configuration.startMaximized = true;
@@ -17,11 +19,16 @@ public class LambdaAllureTests {
 
     @Test
     public void  testIssuePresentInRepoLambda(){
+        SelenideAllureTests allureTests = new SelenideAllureTests();
+
         open("https://github.com");
         $("input[name='q']").click();
-        $("input[name='q']").sendKeys("/Syrlic/guru_allure/issues");
+        $("input[name='q']").sendKeys(REPOSITORY);
         $("input[name='q']").submit();
-        $("div[area_label='Issues]").shouldHave(Condition.text("#1"));
+        $(".repo-list").$("a").click();
+        $("span[data-content='Issues']").click();
+        $("#issue_1").shouldHave(Condition.text("#1"));
+                //.shouldHave(Condition.text("#1"));
 
     }
 }
